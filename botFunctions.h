ddcb79000d8edcc3f68e51c7dd408af4c56a80d4
xaizko <tonytran177013@gmail.com>
2025-05-23 10:55:23 -0400

    Changing to reusable approach

---
+++ botFunctions.h
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <curl/curl.h>
+#include <libwebsockets.h> 
+#include <jansson.h>
+
+void call_discord_api(const char *botToken) {
+    CURL *curl = curl_easy_init();
+    if (!curl) {
+        fprintf(stderr, "Failed to initialize CURL\n");
+        return;
+    }
+
+    struct curl_slist *headers = NULL;
+    char authHeader[256];
+    snprint(authHeader, sizeof(authHeader), "Authorization: Bot %s", botToken);
+
+    //build out header request
+    headers = curl_slist_append(headers, authHeader);
+    headers = curl_slist_append(headers, "Content-Type: ");
+}
--- receive.c
+++ receive.c
@@ -1,14 +1,5 @@
 // discordo_clone.c
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdbool.h>
-#include <stdint.h>
-#include <unistd.h>
-#include <pthread.h>
-#include <curl/curl.h>
-#include <libwebsockets.h> 
-#include <jansson.h>
+#include "botFunctions.h"
 
 int main() {
 
@@ -17,18 +8,3 @@ int main() {
     return 0;
 }
 
-void call_discord_api(const char *botToken) {
-    CURL *curl = curl_easy_init();
-    if (!curl) {
-        fprintf(stderr, "Failed to initialize CURL\n");
-        return;
-    }
-
-    struct curl_slist *headers = NULL;
-    char authHeader[256];
-    snprint(authHeader, sizeof(authHeader), "Authorization: Bot %s", botToken);
-
-    //build out header request
-    headers = curl_slist_append(headers, authHeader);
-    headers = curl_slist_append(headers, "Content-Type: ");
-}
